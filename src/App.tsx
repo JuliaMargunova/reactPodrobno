c2b9f4cafd48bcd0bed9742c478b09a54b6a48d8
Julia <yulia.bobko@mail.ru>
2023-04-21 14:26:12 +0300

    added UncontrolledAccordion

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UnvontrolledRating} from "./components/UnvontrolledRating/UnvontrolledRating";
 
 
 function App() {
     const [isOnOff, setOnOff] = useState<boolean>(false)
     return (
         <div>
+            <UnvontrolledRating />
+            <UncontrolledAccordion title={'Julia syper'} />
             <PageTitle title="App Title"/>
             Article 1
             <Accordion title={"Menu"} collapsed={true}/>
---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    title: string
+}
+
+export default function UncontrolledAccordion(props: AccordionPropsType) {
+
+    const [collapsed,setCollapsed]= useState<boolean>(false);
+    return (
+        <div>
+            <AccordionTitle title={props.title} onClinckAccordionTitle={()=>setCollapsed(!collapsed)}/>
+            {collapsed && <AccordionBody/>}
+        </div>
+    );
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>);
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    onClinckAccordionTitle:()=>void
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3 onClick={props.onClinckAccordionTitle}>{props.title}</h3>;
+}
+
