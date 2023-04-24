225c8750f92ed3df8bfc3296b4fa3ed1f0b13f01
Julia <yulia.bobko@mail.ru>
2023-04-24 17:02:06 +0300

    added UncontrolledOnOff

---
+++ src/components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import styles from './UncontrolledOnOff.module.css'
+
+
+export const UncontrolledOnOff = () => {
+    const [isOn,setOn]= useState<boolean>(false)
+
+    const onStyle = {
+        display: 'inline-block',
+        border: '1px solid ',
+        backgroundColor: isOn ? "green" : "grey",
+        padding: '2px',
+        marginLeft: '5px'
+    }
+
+    const offStyle = {
+        display: 'inline-block',
+        border: '1px solid ',
+        backgroundColor: isOn ? "grey" : "red",
+        padding: '2px'
+    }
+
+    const circle = {
+        height: '15px',
+        width: '15px',
+        borderRadius: '10px',
+        display: 'inline-block',
+        backgroundColor: isOn ? 'green' : 'red',
+        marginLeft: '5px'
+    }
+    return (
+        <div>
+            <div style={onStyle} onClick={()=>setOn(true)}>On
+            </div>
+            <div style={offStyle} onClick={()=>setOn(false)}>Off
+            </div>
+            <div style={circle}></div>
+        </div>
+    )
+
+}
