924a3a97d4b7e3055355e0eeb5b9cfedf558f205
Julia <yulia.bobko@mail.ru>
2023-06-07 19:00:32 +0300

    ControlledSelect  ControlledCheckbox ControlledInput

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -51,4 +51,49 @@ export const GetValueOfUncontrolledInputByButtonPress: Story = {
     render: () => <GetValueOfUncontrolledInputByButtonPressWithHooks/>
 }
 
+const ControlledInputWithHook = () => {
+    const [value, setValue] = useState('')
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+    }
+
+    return <input type="text" value={value} onChange={onChangeHandler}/>
+}
+
+export const ControlledInput: Story = {
+    render: () => <ControlledInputWithHook/>,
+}
+const ControlledCheckboxWithHooks = () => {
+    const [value, setValue] = useState(true)
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.checked)
+    }
+
+    return <input type="checkbox" checked={value} onChange={onChangeHandler}/>
+}
+export const ControlledCheckbox: Story = {
+    render: () => <ControlledCheckboxWithHooks/>,
+}
+const ControlledSelectWithHooks = () => {
+    const [value, setValue] = useState<string | undefined>('2')
+
+    const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+        setValue(event.currentTarget.value)
+    }
+
+    return (
+        <select value={value} onChange={onChangeHandler}>
+            <option>none</option>
+            <option value="1">Minsk</option>
+            <option value="2">Moscow</option>
+            <option value="3">Peter</option>
+        </select>
+    )
+}
+export const ControlledSelect: Story = {
+    render: () => <ControlledSelectWithHooks/>,
+}
+
 
